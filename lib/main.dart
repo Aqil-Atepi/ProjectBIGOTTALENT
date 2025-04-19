06b2c3f27fb893b04a7888a64f7f35707462ad42
Aqil-Atepi <167964589+Aqil-Atepi@users.noreply.github.com>
2025-04-19 17:23:02 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -5,9 +5,9 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Supabase.initialize(
-    url: 'https://fdfyezphpkpaearyiecg.supabase.co',
+    url: '',
     anonKey:
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZkZnllenBocGtwYWVhcnlpZWNnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDUwMjU3MTUsImV4cCI6MjA2MDYwMTcxNX0.zaMqh8GXbLPBi7_VJuZVw74YjUXc-S5ANxLh9Iwk8h8',
+        '',
   );
   runApp(const MyApp());
 }
